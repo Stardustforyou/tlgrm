b85e771674a4303d414231e9da6685413b8d48df
Stardustforyou <32360552+Stardustforyou@users.noreply.github.com>
2017-12-17 06:19:35 +0400

    Update Alex.py

--- Alex.py
+++ Alex.py
@@ -1,11 +1,11 @@
 import telebot
 
-TOKEN = '507137151:AAFGTnLPTJEqZzmlYZhbA0Tm_9pnOGmy_zg' 
+TOKEN = 'да это же токен' 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    sent = bot.send_message(message.chat.id, 'Привет, как к тебе обращаться??')
+    sent = bot.send_message(message.chat.id, 'Привет, как к тебе обращаться?')
     bot.register_next_step_handler(sent, hello)
 
 def hello(message):
