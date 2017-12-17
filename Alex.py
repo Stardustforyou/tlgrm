353574c4de2eba65c3f151f490543ac516fa5b8e
Stardustforyou <32360552+Stardustforyou@users.noreply.github.com>
2017-12-17 06:15:02 +0400

    Add files via upload

---
+++ Alex.py
@@ -0,0 +1,14 @@
+import telebot
+
+TOKEN = '507137151:AAFGTnLPTJEqZzmlYZhbA0Tm_9pnOGmy_zg' # полученный у @BotFather
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    sent = bot.send_message(message.chat.id, 'Привет, как к тебе обращаться??')
+    bot.register_next_step_handler(sent, hello)
+
+def hello(message):
+    bot.send_message(message.chat.id, '{name}, значит. Рад тебя видеть!'.format(name=message.text))
+
+bot.polling()    
